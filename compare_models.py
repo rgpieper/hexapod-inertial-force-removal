640642ba4d1e69e1da498786c42cdf5e1467b389
Riley Pieper <rgpieper@umich.edu>
2025-11-17 14:03:01 -0500

    feat: Add LSTM to comparisons

--- compare_models.py
+++ compare_models.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from format_data import load_perts_h5
 from mlp_model import BasicMLP, calc_avg_vaf
+from lstm_model import MIMOLSTM
 import matplotlib.pyplot as plt
 import IPython
 
@@ -20,7 +21,6 @@ if __name__ == "__main__":
     output_dim = force_chans*window_size
     
     data_file = "data/noLoadPerts_131125.h5"
-    model_file = "models/mlp_models_20251114_005049/mlp_general.pth"
 
     accel_segs, force_segs, meta_data = load_perts_h5(data_file)
 
@@ -39,16 +39,27 @@ if __name__ == "__main__":
     print(val_meta_df)
     pd.set_option('display.max_rows', default_rows)
 
-
-    Model = BasicMLP(
+    mlpmodel_file = "models/mlp_models_20251114_005049/mlp_general.pth"
+    MLPModel = BasicMLP(
         input_dim=input_dim,
         output_dim=output_dim
     )
-    Model.load_weights(model_file)
-
-    predictions = Model.predict_segments(
+    MLPModel.load_weights(mlpmodel_file)
+    predictions_mlp = MLPModel.predict_segments(
         inputs=val_accel_segments,
-        step_size=50
+        step_size=step_size
+    )
+
+    lstmmodel_file = "models/lstm_models_###/lstm_general.pth"
+    LSTMModel = MIMOLSTM(
+        input_size=accel_chans,
+        output_size=force_chans
+    )
+    LSTMModel.load_weights(lstmmodel_file)
+    predictions_lstm = LSTMModel.predict_segments(
+        inputs=val_accel_segments,
+        window_size=window_size,
+        step_size=step_size
     )
 
     # plot predictions
@@ -57,7 +68,8 @@ if __name__ == "__main__":
     seg_slice = range(0,1000)
 
     measurement = val_force_segments[seg_idx][seg_slice,:]
-    prediction = predictions[seg_idx][seg_slice,:]
+    prediction_mlp = predictions_mlp[seg_idx][seg_slice,:]
+    prediction_lstm = predictions_lstm[seg_idx][seg_slice,:]
     seg_meta_data = val_meta_data[seg_idx]
     seg_direction = "forward" if seg_meta_data["dir"] == 1 else "reverse"
 
@@ -73,7 +85,8 @@ if __name__ == "__main__":
             other_force_segs.append(match_force_segs[i])
     avg_seg = np.mean(np.stack(other_force_segs), axis=0)[seg_slice,:]
 
-    vaf_pred = calc_avg_vaf([torch.from_numpy(prediction).float()], [torch.from_numpy(measurement).float()])
+    vaf_pred_mlp = calc_avg_vaf([torch.from_numpy(prediction_mlp).float()], [torch.from_numpy(measurement).float()])
+    vaf_pred_lstm = calc_avg_vaf([torch.from_numpy(prediction_lstm).float()], [torch.from_numpy(measurement).float()])
     vaf_avg = calc_avg_vaf([torch.from_numpy(avg_seg).float()], [torch.from_numpy(measurement).float()])
 
     rows = 4
@@ -93,7 +106,8 @@ if __name__ == "__main__":
 
             ax.plot(measurement[:,chan_idx], label="Measurement")
             ax.plot(avg_seg[:,chan_idx], label=f"Averaged: {vaf_avg:.01f}%", linestyle="--")
-            ax.plot(prediction[:,chan_idx], label=f"Prediction: {vaf_pred:.01f}%")
+            ax.plot(prediction_lstm[:,chan_idx], label=f"LSTM Prediction: {vaf_pred_lstm:.01f}%")
+            ax.plot(prediction_mlp[:,chan_idx], label=f"MLP Prediction: {vaf_pred_mlp:.01f}%")
 
             if i==0 and j==0:
                 ax.legend()
